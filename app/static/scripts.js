cc2a1ab07d99c118032d1dc6d0035b8bd5e49f08
Du Park <dupark3@gmail.com>
2018-09-17 15:44:30 -0400

    Commented out fade in of body elements

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -3,16 +3,14 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "0px";
-    document.getElementById("navbar").style.backgroundColor = "var(--grey)";
     document.getElementById("navbar-right").style.padding = "5px 10px";
     document.getElementById("logo-image").style.height = "40px";
   } else {
-    document.getElementById("navbar").style.padding = "15x 10px";
-    document.getElementById("navbar").style.backgroundColor = "transparent";
+    document.getElementById("navbar").style.padding = "0.5em";
     document.getElementById("navbar-right").style.padding = "30px 10px";
     document.getElementById("logo-image").style.height = "75px";
   }
-  if (document.body.scrollTop > 480 || document.documentElement.scrollTop > 480) {
+/*  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.getElementById("today").style.opacity = "1.0";
   } else {
     document.getElementById("today").style.opacity = "0";
@@ -21,5 +19,5 @@ function scrollFunction() {
     document.getElementById("subscribe").style.opacity = "1.0";
   } else {
     document.getElementById("subscribe").style.opacity = "0";
-  }
+  }*/
 }
