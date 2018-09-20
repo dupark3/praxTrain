d7c9c642489762698a4e74440f212298b453b79c
Du Park <dupark3@gmail.com>
2018-09-20 13:05:32 -0400

    Keep track of last time key was refreshed.

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,11 +1,18 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import datetime
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open('PraxTrainSheet').sheet1
+lastRefreshTime = datetime.datetime.now()
+
+def refreshKey():
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    lastRefreshTime = datetime.datetime.now()
 
 def getSpreadsheet():    
     return sheet.get_all_records()
