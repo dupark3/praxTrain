a3d144f696aa6c5046c3c003ec9b4236f1e1c682
Du Park <dupark3@gmail.com>
2018-10-05 10:44:38 -0400

    Added confirmation emails being sent. Added email verification upon subscription. Added unsubscribe links. Added confirmation email html generation.

--- app/views.py
+++ app/views.py
@@ -9,39 +9,15 @@ import spreadsheet # spreadsheet.py
 import admininfo # admininfo.py
 import dailyemail
 
-import datetime
-import sqlite3
+import datetime, sqlite3, threading, re
 from werkzeug.security import check_password_hash
-import threading
-
+from itsdangerous import URLSafeSerializer
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        msg = ""
-        try:
-            firstName = request.form['firstName']
-            lastName = request.form['lastName']
-            email = request.form['email']
-            
-            con = sqlite3.connect("database.db")
-            cur = con.cursor()
-            cur.execute('''INSERT INTO subscribers (firstName, lastName, email)
-                VALUES (?,?,?)''',(firstName, lastName, email) )
-            con.commit()
-            msg = "Record successfully added"
-        except:
-            con.rollback()
-            msg = "error in insert operation"
-        finally:
-            return render_template('subscribed.html', msg=msg)
-            con.close()
-    
-    # METHOD = 'GET'    
-    else:
-        return render_template('index.html', 
-                               records=spreadsheet.getSpreadsheet(), 
-                               indexToday=spreadsheet.getTodayIndex())
+    return render_template('index.html', 
+                records=spreadsheet.getSpreadsheet(), 
+                indexToday=spreadsheet.getTodayIndex())
 
 @app.route('/recent', methods=['GET'])
 def recent():
@@ -49,13 +25,127 @@ def recent():
                            records=spreadsheet.getSpreadsheet(), 
                            indexToday=spreadsheet.getTodayIndex())    
 
-@app.route('/subscribed', methods=['GET'])
+@app.route('/subscribed', methods=['POST'])
 def subscribed():
-    return render_template('subscribed.html')
+    msg = ""
+    firstName = request.form['firstName']
+    lastName = request.form['lastName']
+    email = request.form['email'].lower()
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return render_template('subscribed.html', 
+            msg="Invalid email address. Please try again.", 
+            records=spreadsheet.getSpreadsheet(), 
+            indexToday=spreadsheet.getTodayIndex())
+    elif firstName == '':
+        return render_template('subscribed.html', 
+            msg="Please enter a first name.", 
+            records=spreadsheet.getSpreadsheet(), 
+            indexToday=spreadsheet.getTodayIndex())
+    elif lastName == '':
+        return render_template('subscribed.html', 
+            msg="Please enter a last name.", 
+            records=spreadsheet.getSpreadsheet(), 
+            indexToday=spreadsheet.getTodayIndex())
+    try:
+        con = sqlite3.connect("database.db")
+        cur = con.cursor()
+        
+        # Check if email already exists
+        cur.execute('''SELECT * FROM subscribers WHERE email="{}"'''.format(email))
+        rows = cur.fetchall()
+        if rows:
+            if rows[0][4] == 0:
+                msg = "This email is already subscribed. A new confirmation email has been sent."
+                dailyemail.send_confirmation(email)
+            elif rows[0][4] == 1:
+                msg = "This email has already been subscribed and confirmed."
+            return render_template('subscribed.html', msg=msg, records=spreadsheet.getSpreadsheet(), 
+                               indexToday=spreadsheet.getTodayIndex())
+
+        # Email does not exist, insert into table
+        cur.execute('''INSERT INTO subscribers (firstName, lastName, email)
+            VALUES (?,?,?)''',(firstName, lastName, email) )
+        con.commit()
+        msg = "You have been successfully subscribed. Please check your email for a confirmation email."
+    except:
+        con.rollback()
+        msg = "There was an error in your subscription."
+    finally:
+        dailyemail.send_confirmation(email)
+        return render_template('subscribed.html', msg=msg, records=spreadsheet.getSpreadsheet(), 
+                           indexToday=spreadsheet.getTodayIndex())
+        con.close()
+
+@app.route('/unsubscribe/<token>', methods=['GET'])
+def unsubscribe(token):
+    serializer = URLSafeSerializer(admininfo.secret_key, salt=admininfo.unsubscribe_salt)
+    # add try-catch statement here with itsdangerous.BadData
+    email = serializer.loads(token)
+
+    # Check if valid unsubscribe url
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return render_template('subscribed.html', 
+            msg="Invalid URL. Please check your unsubscribe email address again. Contact the admin if unable to unsubscribe.", 
+            records=spreadsheet.getSpreadsheet(), 
+            indexToday=spreadsheet.getTodayIndex())
+    msg = ""
+    try:
+        con = sqlite3.connect("database.db")
+        cur = con.cursor()
+        cur.execute('''DELETE FROM subscribers 
+            WHERE email="{}";'''.format(email))
+        con.commit()
+        msg = email + " has been successfully unsubscribed from PraxTrain."
+    except:
+        con.rollback()
+        msg = "There was an error in your unsubscription."
+    finally:
+        return render_template('subscribed.html', msg=msg, records=spreadsheet.getSpreadsheet(), 
+                               indexToday=spreadsheet.getTodayIndex())
+        con.close()
+    
+
+@app.route('/confirm/<token>', methods=['GET'])
+def confirm(token):
+    serializer = URLSafeSerializer(admininfo.secret_key, salt=admininfo.confirm_salt)
+    # add try-catch statement here with itsdangerous.BadData
+    email = serializer.loads(token)
+    # Check if valid unsubscribe url
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return render_template('subscribed.html', 
+            msg="Invalid URL. Please check your confirmation email address again. Contact the admin if unable to confirm.", 
+            records=spreadsheet.getSpreadsheet(), 
+            indexToday=spreadsheet.getTodayIndex())
+    msg = ""
+    try:
+        con = sqlite3.connect("database.db")
+        cur = con.cursor()
+        cur.execute('''UPDATE subscribers SET confirmed=1
+            WHERE email="{}";'''.format(email))
+        con.commit()
+        msg = email + " has been successfully confirmed."
+    except:
+        con.rollback()
+        msg = "There was an error in your confirmation."
+    finally:
+        return render_template('subscribed.html', msg=msg, records=spreadsheet.getSpreadsheet(), 
+                               indexToday=spreadsheet.getTodayIndex())
+        con.close()
 
 @app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        dailyemail.send_contact_form(request.form['firstName'], request.form['lastName'], request.form['email'], request.form['comments'])
+        return render_template('subscribed.html', msg='Your comments have been emailed to the PraxTrain team. We will get back to you as soon as possible.', 
+                    records=spreadsheet.getSpreadsheet(), 
+                    indexToday=spreadsheet.getTodayIndex())
+    else:
+        return render_template('contact.html')
+
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
@@ -65,11 +155,23 @@ def admin():
         cur.execute('SELECT * FROM subscribers')
         rows = cur.fetchall()
         con.close()
-        return render_template('admin.html', rows=rows)
+        today = datetime.datetime.now()
+        
+        serializer = URLSafeSerializer(admininfo.secret_key, salt=admininfo.unsubscribe_salt)
+        unsubscribe_links = ['http://praxtrain.com/unsubscribe/' + serializer.dumps(row[3]) for row in rows]
+        serializer = URLSafeSerializer(admininfo.secret_key, salt=admininfo.confirm_salt)
+        confirm_links = ['http://praxtrain.com/confirm/' + serializer.dumps(row[3]) for row in rows]
+        return render_template('admin.html', rows=rows, unsubscribe_links=unsubscribe_links, confirm_links=confirm_links, hour=today.hour, minute=today.minute, second=today.second)
     else:
         return render_template('adminlogin.html')
 
+@app.route('/manualemail', methods=['POST'])
+def manualemail():
+    dailyemail.send_emails()
+    return render_template('subscribed.html', msg='Emails sent to subscribers manually.', records=spreadsheet.getSpreadsheet(), 
+                               indexToday=spreadsheet.getTodayIndex())
+
 if __name__ == '__main__':
-    threadObj = threading.Thread(target=dailyemail.server)
+    threadObj = threading.Thread(target=dailyemail.send_confirmation, args=['dupark3@gmail.com'])
     threadObj.start()
     app.run()
