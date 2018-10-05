f39521912f350b08a49b69ab38e2a6c78065de86
Du Park <dupark3@gmail.com>
2018-10-05 14:19:43 -0400

    Added jquery for fading out elements. Fixed mobile touch not triggering modal close with jquery .on('click touchstart').

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -47,9 +47,37 @@ for (var i = 0; i < btns.length; i++){
   }
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// Open modal if myBtn clicked. Close modal if clicked outside of modalContent
+$(document).ready(function(){
+    $('.myBtn').on('click touchstart', function() {
+      $('#myModal').css('display','block');
+      // Wait until display transition is done to focus
+      setTimeout(function(){
+          $('#firstNameSub').focus();
+      });
+    });
+    $("#myModal").on('click touchstart', function(close) {
+      if (close.target.id == "modal-content" || $(close.target).parents("#modal-content").length) {
+        $('#myModal').css('display','block');
+      } else {
+        $('#myModal').css('display','none');
+      }
+    });
+});
+
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
